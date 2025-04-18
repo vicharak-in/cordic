c7d3802080091381d1354130b656f41930362922
Devyani-EC <devyani29r@gmail.com>
2025-04-18 15:50:35 +0530

    feat(cordic): Separated modes for improved input selection
    - Updated mode selection logic to allow independent execution of sin and
      cos operations.
    - Previously, mode 1 returned both outputs together; now, sin and cos can be
      selected separately via different modes.
      and same for the sinh/cosh and arcsin/arccos.
    - changed cordic_clk from 19Mhz to 15Mhz.
    - Modified README to reflect the updated input/output mode mapping and frame structures.

--- ip/cordic_tanh/cordic_tanh_tmpl.vhd
+++ ip/cordic_tanh/cordic_tanh_tmpl.vhd
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- Copyright (C) 2013-2023 Efinix Inc. All rights reserved.              
+-- Copyright (C) 2013-2024 Efinix Inc. All rights reserved.              
 --
 -- This   document  contains  proprietary information  which   is        
 -- protected by  copyright. All rights  are reserved.  This notice       
@@ -37,34 +37,37 @@
 --
 --------------------------------------------------------------------------------
 ------------- Begin Cut here for COMPONENT Declaration ------
-COMPONENT cordic_tanh is
-PORT (
-clk : in std_logic;
-reset_n : in std_logic;
-i_call : in std_logic;
-i_data : in std_logic_vector(31 downto 0);
-o_done : out std_logic;
-o_cosh : out std_logic_vector(31 downto 0);
-o_sinh : out std_logic_vector(31 downto 0);
-o_tanh : out std_logic_vector(31 downto 0);
-o_alpha : out std_logic_vector(31 downto 0);
-o_y : out std_logic_vector(31 downto 0);
-o_x : out std_logic_vector(31 downto 0));
-END COMPONENT;
----------------------- End COMPONENT Declaration ------------
+component cordic_tanh is
+port (
+    clk : in std_logic;
+    reset_n : in std_logic;
+    i_call : in std_logic;
+    i_data : in std_logic_vector(31 downto 0);
+    o_done : out std_logic;
+    o_cosh : out std_logic_vector(31 downto 0);
+    o_sinh : out std_logic_vector(31 downto 0);
+    o_tanh : out std_logic_vector(31 downto 0);
+    o_alpha : out std_logic_vector(31 downto 0);
+    o_y : out std_logic_vector(31 downto 0);
+    o_x : out std_logic_vector(31 downto 0)
+);
+end component cordic_tanh;
 
+---------------------- End COMPONENT Declaration ------------
 ------------- Begin Cut here for INSTANTIATION Template -----
 u_cordic_tanh : cordic_tanh
-PORT MAP (
-clk => clk,
-reset_n => reset_n,
-i_call => i_call,
-i_data => i_data,
-o_done => o_done,
-o_cosh => o_cosh,
-o_sinh => o_sinh,
-o_tanh => o_tanh,
-o_alpha => o_alpha,
-o_y => o_y,
-o_x => o_x);
+port map (
+    clk => clk,
+    reset_n => reset_n,
+    i_call => i_call,
+    i_data => i_data,
+    o_done => o_done,
+    o_cosh => o_cosh,
+    o_sinh => o_sinh,
+    o_tanh => o_tanh,
+    o_alpha => o_alpha,
+    o_y => o_y,
+    o_x => o_x
+);
+
 ------------------------ End INSTANTIATION Template ---------
